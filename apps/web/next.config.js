162dc48df9da01df7b1e761846bdbe012f3d9019
1M4nt0 <andrea.antonutti@gmail.com>
2023-05-28 21:18:44 +0200

    fix: Fix prisma.schema not found

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 module.exports = {
     reactStrictMode: true,
-    transpilePackages: ["@dae/ui"],
-    webpack: (config) => {
-        config.resolve.fallback = { fs: false, net: false, tls: false, encoding: false };
-        return config;
+    transpilePackages: ['@dae/ui'],
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
+        config.resolve.fallback = { fs: false, net: false, tls: false, encoding: false }
+        return config
     },
-};
+}
