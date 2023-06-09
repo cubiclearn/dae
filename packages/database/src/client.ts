aed853b6280cf09d3d74613a37581933fa52e181
1M4nt0 <andrea.antonutti@gmail.com>
2023-06-09 22:58:23 +0200

    check

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -4,7 +4,9 @@ declare global {
   var globalPrisma: PrismaClient | undefined
 }
 
-export const prisma = global.globalPrisma || new PrismaClient()
+export const prisma = global.globalPrisma || new PrismaClient({
+  log: ['query']
+})
 
 if (process.env.NODE_ENV !== 'production') global.globalPrisma = prisma
 
